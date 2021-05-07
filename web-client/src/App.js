9bb3a729fc29fec97f11620277b59dbdfe21a001
Roshen Nair <roshen620@gmail.com>
2021-05-07 22:20:09 +0800

    Complete CSS styling for signup form component

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,9 +1,12 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import SignUp from './components/SignUp';
 
 const App = () => {
     return (
         <Router>
+            <Route path='/'>
+                <Redirect to='/sign-up' />
+            </Route>
             <Route path='/sign-up'>
                 <SignUp />
             </Route>
